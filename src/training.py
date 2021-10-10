a12e5cc705c2aa457ac2506ed7b652089aaa0c0b
Suresh-Singamsetty <sureshsmart96@gmail.com>
2021-10-10 18:54:40 +0530

    python package created

--- src/training.py
+++ src/training.py
@@ -5,9 +5,10 @@ from utils.plot import save_plot
 import argparse
 import os
 
+
 def training(config_path):
     config = read_config(config_path)
-    
+
     validation_datasize = config["params"]["validation_datasize"]
     (X_train, y_train), (X_valid, y_valid), (X_test, y_test) = get_data(validation_datasize)
 
@@ -22,31 +23,31 @@ def training(config_path):
     VALIDATION_SET = (X_valid, y_valid)
 
     history = model.fit(X_train, y_train, epochs=EPOCHS,
-                    validation_data=VALIDATION_SET)
+                        validation_data=VALIDATION_SET)
 
     artifacts_dir = config["artifacts"]["artifacts_dir"]
     model_dir = config["artifacts"]["model_dir"]
-    
+
     model_dir_path = os.path.join(artifacts_dir, model_dir)
     os.makedirs(model_dir_path, exist_ok=True)
-    
+
     model_name = config["artifacts"]["model_name"]
 
     save_model(model, model_name, model_dir_path)
     plot_dir = config["artifacts"]["plots_dir"]
     plot_dir_path = os.path.join(artifacts_dir, plot_dir)
-    os.makedirs(plot_dir_path, exist_ok= True)
+    os.makedirs(plot_dir_path, exist_ok=True)
 
     loss_acc = history.history
     plot_name = config["artifacts"]["plot_name"]
-    save_plot(loss_acc,plot_name,plot_dir_path)
+    save_plot(loss_acc, plot_name, plot_dir_path)
 
 
 if __name__ == "__main__":
-	args = argparse.ArgumentParser()
+    args = argparse.ArgumentParser()
 
-	args.add_argument("--config","-c", default="config.yaml")
+    args.add_argument("--config", "-c", default="config.yaml")
 
-	parsed_args = args.parse_args()
+    parsed_args = args.parse_args()
 
-	training(config_path = parsed_args.config)
+    training(config_path=parsed_args.config)
